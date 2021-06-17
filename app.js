0465de223fec4380e8cf92fd1fc78103d49ade8b
t0kar <81991067+t0kar@users.noreply.github.com>
2021-06-17 20:43:56 +0200

    BONUS update

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-function findFizzBuzz(max) {
+function findFizzBuzz(min, max, modulo_3_msg = "fizz", modulo_5_msg = "buzz") {
     var fizzBuzz = [];
 
-    for (var i = 0; i <= max; i++) {
+    for (var i = min; i <= max; i++) {
         if (i === 0) continue;
 
         var message = "";
 
-        if (i % 3 === 0) message += "fizz";
-        if (i % 5 === 0) message += "buzz";
+        if (i % 3 === 0) message += modulo_3_msg;
+        if (i % 5 === 0) message += modulo_5_msg;
 
         if (message !== "") fizzBuzz.push(message);
     }
@@ -15,8 +15,8 @@ function findFizzBuzz(max) {
     return fizzBuzz;
 }
 
-var fizzBuzz1 = findFizzBuzz(20);
+var fizzBuzz1 = findFizzBuzz(5, 20, "diss", "miss");
 console.log(fizzBuzz1, "array length is", fizzBuzz1.length);
-var fizzBuzz2 = findFizzBuzz(40);
+var fizzBuzz2 = findFizzBuzz(10, 80, "riss", "kiss");
 console.log(fizzBuzz2, "array length is", fizzBuzz2.length);
 
