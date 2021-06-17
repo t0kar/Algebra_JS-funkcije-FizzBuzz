0c1bc2212b5e53ea524875cddf2f48b8561008e6
t0kar <81991067+t0kar@users.noreply.github.com>
2021-06-17 20:37:06 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 function findFizzBuzz(max) {
     var fizzBuzz = [];
+    var array_length;
 
     for (var i = 0; i <= max; i++) {
         if (i === 0) continue;
@@ -15,5 +16,8 @@ function findFizzBuzz(max) {
     return fizzBuzz;
 }
 
-console.log(findFizzBuzz(20));
-console.log(findFizzBuzz(40));
+var fizzBuzz1 = findFizzBuzz(20);
+console.log(fizzBuzz1, "array length is", fizzBuzz1.length);
+var fizzBuzz2 = findFizzBuzz(40);
+console.log(fizzBuzz2, "array length is", fizzBuzz2.length);
+
